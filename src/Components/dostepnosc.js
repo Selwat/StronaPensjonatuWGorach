da9936d4b75fb65c1f701a7b40bd7a3c19b0bc38
Mikołaj Selwat <92488579+Selwat@users.noreply.github.com>
2023-03-21 19:58:23 +0100

    Creation of a system for selecting the date

--- src/Components/dostepnosc.js
+++ src/Components/dostepnosc.js
@@ -3,84 +3,83 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
 const Dostepnosc = () => {
-const [selectedMonth, setSelectedMonth] = useState(null);
-const [selectedDay, setSelectedDay] = useState(null);
-const [isDostepnoscVisible, setDostepnoscVisible] = useState(false);
-const [isResetVisible, setResetVisible] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [isDostepnoscVisible, setDostepnoscVisible] = useState(false);
+  const [isResetVisible, setResetVisible] = useState(false);
+  const [isWymeldowanieVisible, setWymeldowanieVisible] = useState(false);
 
-const handleMonthChange = (date) => {
-setSelectedMonth(date);
-};
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
 
-const handleDayChange = (date) => {
-setSelectedDay(date);
-};
+  const renderDatePicker = () => {
+    return (
+      <DatePicker
+        selected={selectedDate}
+        onChange={handleDateChange}
+        dateFormat="dd.MM.yyyy"
+        minDate={new Date()}
+      />
+    );
+  };
 
-const renderMonthPicker = () => {
-return (
-<DatePicker
-     selected={selectedMonth}
-     onChange={handleMonthChange}
-     dateFormat="MMMM yyyy"
-     showMonthYearPicker
-   />
-);
-};
+  const handleDostepnoscClick = () => {
+    setDostepnoscVisible(true);
+    setResetVisible(true);
+  };
 
-const renderDayPicker = () => {
-return (
-<DatePicker
-selected={selectedDay}
-onChange={handleDayChange}
-dateFormat="dd.MM.yyyy"
-minDate={selectedMonth && new Date(selectedMonth.getFullYear(), selectedMonth.getMonth(), 1)}
-maxDate={selectedMonth && new Date(selectedMonth.getFullYear(), selectedMonth.getMonth() + 1, 0)}
-/>
-);
-};
+  const handleResetClick = () => {
+    setSelectedDate(null);
+    setDostepnoscVisible(false);
+    setResetVisible(false);
+    setWymeldowanieVisible(false);
+  };
+  /////////////////////////
+  const handleWymeldujClick = () => {
+    setWymeldowanieVisible(true);
+    setResetVisible(true);
+  };
 
-const handleDostepnoscClick = () => {
-setDostepnoscVisible(true);
-setResetVisible(true);
-};
 
-const handleResetClick = () => {
-setSelectedMonth(null);
-setSelectedDay(null);
-setDostepnoscVisible(false);
-setResetVisible(false);
-};
 
-return (
-<div className="DostepnoscDiv">
-<h2>Dostępność</h2>
-<div className="DostepnoscUwaga">
-<span class="material-symbols-outlined">error</span>
-<p>Wybierz daty, aby zobaczyć dostępność i ceny w obiekcie</p>
-</div>
-<div className="OknoDostepnosci" style={{ display: isDostepnoscVisible ? 'block' : 'none' }}>
-<div className="input-container">
-<label>Wybierz miesiąc:</label>
-{renderMonthPicker()}
-</div>
-{selectedMonth && (
-<div className="input-container">
-<label>Wybierz dzień:</label>
-{renderDayPicker()}
-</div>
-)}
-</div>
-{isResetVisible ? (
-<button className="DostepnoscReset" onClick={handleResetClick}>
-Resetuj
-</button>
-) : (
-<button className="DostepnoscData" onClick={handleDostepnoscClick}>
-Wybierz date
-</button>
-)}
-</div>
-);
+
+  return (
+    <div className="DostepnoscDiv">
+      <h2>Dostępność</h2>
+      <div className="DostepnoscUwaga">
+        <span class="material-symbols-outlined">error</span>
+        <p>Wybierz daty, aby zobaczyć dostępność i ceny w obiekcie</p>
+      </div>
+
+      <div className="OknoWyboruDaty">
+
+        <div className="OknoDostepnosci" style={{ display: isDostepnoscVisible ? 'block' : 'none' }}>
+          <div className="input-container">
+            <label>Wybierz datę zameldowania:</label>
+            {renderDatePicker()}
+          </div>
+        </div>
+
+        <div className="OknoWymeldowania" style={{ display: isWymeldowanieVisible ? 'block' : 'none' }}>
+          <div className="input-container">
+            <label>Wybierz datę wymeldowania:</label>
+            {renderDatePicker()}
+          </div>
+        </div>
+        
+        {isDostepnoscVisible ? (
+          <button className="DostepnoscReset" onClick={handleResetClick}>Resetuj</button>
+        ) : (
+          <button className="DostepnoscData" onClick={handleDostepnoscClick}>Zameldowanie</button>
+          
+        )}
+        {isWymeldowanieVisible ? '' : (
+          <button className="WymeldowanieData"  onClick={handleWymeldujClick}>Wymeldowanie</button>
+        )}
+        
+      </div>
+    </div>
+  );
 };
 
 export default Dostepnosc;
